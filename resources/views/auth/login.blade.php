4859f7e785ac796d1a0fba4da9ca29639ad52513
alex <san1295@tut.by>
2021-02-17 11:21:19 +0300

    upd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-7 card__login">
+    <div class="row justify-content-center" style="
+                    height: 96vh;
+                    
+                ">
+        <div class="col-md-7 card__login" >
             <div class="card">
                 <div class="card-header header-login">{{'ВХОД В АДМИН ПАНЕЛЬ' }}</div>
 
@@ -29,30 +32,27 @@
                                     </span>
                                 @enderror
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Запомнить') }}
-                                    </label>
-                                </div>
+                        <div class="card__controls">
+                            <div class="form-group card__control">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+    
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Запомнить') }}
+                                        </label>
+                                    </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="button button__indigo" style="max-width: 140px; width: 100%;">
-                                    {{ __('Войти') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+    
+                            <div class="form-group card__control">
+                                <button type="submit" class="button button__indigo" style="max-width: 240px; width: 100%;">
+                                        {{ __('Войти') }}
+                                    </button>
+    
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
                             </div>
                         </div>
                     </form>
