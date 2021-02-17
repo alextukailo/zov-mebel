8cfce634ca22cd6e4843a1bbd5e394d0c001fd62
alex <san1295@tut.by>
2021-02-17 10:50:37 +0300

    upd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-7">
+        <div class="col-md-7 card__login">
             <div class="card">
                 <div class="card-header header-login">{{'ВХОД В АДМИН ПАНЕЛЬ' }}</div>
 
@@ -11,20 +11,6 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <!-- <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
-
-                            <div class="col-md-6">
-                                
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div> -->
                         <div class="form__item" style="max-width: 400px; width: 100%; margin: 0 auto; margin-bottom: 20px;">
                             <!-- <div class="form__heading">{{ __('Email') }}</div> -->
                             <div class="form__input login email"><input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Введите email" value="{{ old('email') }}" required autocomplete="email" autofocus></div>
@@ -43,21 +29,6 @@
                                     </span>
                                 @enderror
                         </div>
-
-
-                        <!-- <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Пароль') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div> -->
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
