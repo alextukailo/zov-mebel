826436c9779da8b13f8571fd4a2b083100641fb0
alex <san1295@tut.by>
2021-02-18 11:49:06 +0300

    upd

--- public/js/custom.js
+++ public/js/custom.js
@@ -35292,6 +35292,25 @@ function requests() {
     }
   }; // singleRequestPage()
 
+
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        console.log(e.target.result);
+        $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+        $('#imagePreview').hide();
+        $('#imagePreview').fadeIn(650);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#fileUpload").change(function () {
+    readURL(this);
+  });
 }
 
 /***/ }),
--- resources/js/components/requsts.js
+++ resources/js/components/requsts.js
@@ -32,4 +32,22 @@ export default function requests() {
         }
     }
     // singleRequestPage()
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                console.log(e.target.result)
+                $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#fileUpload").change(function() {
+        readURL(this);
+    });
+
+
 }
